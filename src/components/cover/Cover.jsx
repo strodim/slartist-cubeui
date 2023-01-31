69b12af4e21c9f59b819521553e3a22f3e48e8b6
Dima Stro <strodim3@gmail.com>
2023-01-31 21:24:00 +0200

    Styles changes

--- src/components/cover/Cover.jsx
+++ src/components/cover/Cover.jsx
@@ -1,7 +1,29 @@
+import { useContext } from "react";
+import DeviceContext from "../../context/DeviceContext";
+
 function Cover() {
 
+    const { device } = useContext(DeviceContext);
+    let coverHeight = undefined;
+
+    switch (device) {
+        case 'big':
+            coverHeight = '500px';
+            break;
+        case 'medium':
+            coverHeight = '400px';
+            break;
+        case 'small':
+            coverHeight = '300px';
+            break;
+    }
+
+    const style = {
+        height: coverHeight
+    }
+
     return (
-        <div className="cover">
+        <div className="cover" style={style}>
 
         </div>
     );
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,29 @@
+import DeviceContext from "../../context/DeviceContext";
+import { useContext } from "react";
+
 function Footer() {
 
+    const { device } = useContext(DeviceContext);
+    let height = undefined;
+
+    switch (device) {
+        case 'big':
+            height = '150px';
+            break;
+        case 'medium':
+            height = '120px';
+            break;
+        case 'small':
+            height = '100px';
+            break;
+    }
+
+    const style = {
+        height
+    }
+
     return (
-        <div className="footer">
+        <div className="footer" style={style}>
 
         </div>
     );
